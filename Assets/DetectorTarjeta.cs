c8473cfd862ff93ae5e5821875da97e2df041a5b
Jorge <jgragia@epsg.upv.es>
2022-12-12 16:36:07 +0100

    TEST

---
+++ Assets/DetectorTarjeta.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem
+{
+    public class DetectorTarjeta : MonoBehaviour
+    {
+        [SerializeField]
+        float radio;
+        [SerializeField]
+        GameObject tarjeta;
+        [SerializeField]
+        GameObject tarjetaLinearMap;
+        [SerializeField]
+        Material materialRojo;
+        [SerializeField]
+        Material materialVerde;
+        [SerializeField]
+        GameObject puerta;
+
+        public LinearMapping linearMapping;
+
+        public GameObject prefab;
+
+        [SerializeField]
+        bool leerTarjeta = false;
+        [SerializeField]
+        bool tarjetaLeida = false;
+        // Start is called before the first frame update
+        private void Start()
+        {
+            materialRojo.color = Color.HSVToRGB(0, 1, 1);
+            materialVerde.color = Color.HSVToRGB(0.35f, 1, 0.2f);
+        }
+
+        void Update()
+        {
+            if (!leerTarjeta)
+            {
+                Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, radio);
+                foreach (var hitCollider in hitColliders)
+                {
+                    if (hitCollider.gameObject.transform.parent.gameObject == tarjeta)
+                    {
+                        Debug.Log("Tarjeta");
+                        tarjeta.GetComponent<Rigidbody>().isKinematic = true;
+                        tarjeta.transform.GetChild(0).gameObject.SetActive(false);
+                        tarjetaLinearMap.transform.GetChild(0).gameObject.SetActive(true);
+                        leerTarjeta = true;
+                    }
+                }
+            }
+            else
+            {
+                if(linearMapping.value > 0.9 && tarjetaLeida == false)
+                {
+                    Debug.Log("Hola");
+                    // Se abre la puerta
+                    tarjeta.transform.position = tarjetaLinearMap.transform.position;
+                    tarjeta.GetComponent<Rigidbody>().isKinematic = false;
+                    tarjeta.GetComponent<Rigidbody>().useGravity = true;
+                    tarjeta.transform.GetChild(0).gameObject.SetActive(true);
+                    tarjetaLinearMap.transform.GetChild(0).gameObject.SetActive(false);
+                    tarjetaLeida = true;
+                    StartCoroutine(IEPasarTarjetaLector());
+
+                    //Instantiate(prefab, tarjetaLinearMap.transform.position, Quaternion.identity);
+
+                }
+            }
+
+            IEnumerator IEPasarTarjetaLector()
+            {
+                materialRojo.color = Color.HSVToRGB(0, 1, 0.2f);
+                materialVerde.color = Color.HSVToRGB(0.35f, 1, 1f);
+
+                puerta.GetComponent<Animator>().Play("Puerta");
+                //audioClipManager.SeleccionarAudio(7, 0.5f);
+                yield return new WaitForSeconds(1f);
+                puerta.GetComponent<Animator>().enabled = false; // parar animacion
+
+            }
+        }
+    }
+}
