093e55ba9be8cf1a0f84dcc2a7e73f8bbee3013b
Tostyfis 360 <tostyfis360@gmail.com>
2023-01-11 10:28:59 +0100

    Sincronizar preparar objetos

--- Assets/Scripts/PrepararObjetos.cs
+++ Assets/Scripts/PrepararObjetos.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 namespace Valve.VR.InteractionSystem
 {
@@ -15,9 +16,14 @@ namespace Valve.VR.InteractionSystem
         int contador;
 
         public bool objetos;
+
+
+        private PhotonView photonView;
+
         // Start is called before the first frame update
         void Start()
         {
+            photonView = GetComponent<PhotonView>();
             finNivelScript = FinNivel.Instance;
         }
 
@@ -30,22 +36,28 @@ namespace Valve.VR.InteractionSystem
         void OnTriggerEnter(Collider c)
         {
             Debug.Log(objetosNecesarios.Length);
-            foreach(GameObject objetoNecesario in objetosNecesarios)
+            foreach (GameObject objetoNecesario in objetosNecesarios)
             {
                 if (c.gameObject == objetoNecesario && !objetos)
                 {
-                    Debug.Log("Colision");
-                    objetoNecesario.transform.position = new Vector3(999.0f,999.0f,999.0f);
-                    contador++;                
+                    photonView.RPC("PlayApagarLuces", RpcTarget.All, objetoNecesario);
                 }
             }
-
             if (contador == objetosNecesarios.Length)
             {
                 finNivelScript.FinalizadoPuzlePrepararObjetos();
                 //objetos = true;
             }
 
+        }
+
+        [PunRPC]
+        private void ponerObjetos(GameObject objetoNecesario)
+        {
+
+            objetoNecesario.transform.position = new Vector3(999.0f, 999.0f, 999.0f);
+            contador++;
+            
         }
     }
 }
